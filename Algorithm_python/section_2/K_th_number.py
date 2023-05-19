24173475eaebe6b39e2d0a05e716c4ee4a1c7158
8luerose <8luerose@naver.com>
2023-05-19 19:16:57 +0900

    sec_2 K_th_number

--- Algorithm_python/section_2/K_th_number.py
+++ Algorithm_python/section_2/K_th_number.py
@@ -1,5 +1,16 @@
 T = int(input())
 
+for i in range(T):
+    n, s, e, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    a = a[s-1 : e]
+    a.sort()
+    K_th_number = a[k-1]
+    print(f"#{i+1} {K_th_number}")
+    #print("#%d %d" %(i+1, a[k-1]))		이렇게도 가능하다
+    
+'''
+#기존에 내가 쓴 코드
 
 for i in range(1, T+1):
     N, s, e, k = map(int, input().split())
@@ -9,3 +20,18 @@ for i in range(1, T+1):
     sorted_list = sorted(arr[s-1 : e])
     K_th_number = sorted_list[k-1]
     print(f"#{i} {K_th_number}")
+'''
+
+'''
+입력
+
+2
+6 2 5 3 
+5 2 7 3 8 9 
+15 3 10 3
+4 15 8 16 6 6 17 3 10 11 18 7 14 7 15
+
+출력
+#1 7
+#2 6
+'''
