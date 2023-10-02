f1f7fb6e3c9cb55b844b3995dd52bd5b1ce80c22
8luerose <8luerose@naver.com>
2023-10-03 00:21:46 +0900

    sec_7 05_coin_distribution(DFS) + 해설 추가 +  코드 수정

--- Algorithm_python/section_7/05_coin_distribution(DFS).py
+++ Algorithm_python/section_7/05_coin_distribution(DFS).py
@@ -8,25 +8,40 @@
 	= 예시로 0번에게 준다고 하면, money[i] += 8
 	= D(L + 1)로 가서 또 for 돌면서 누구한테 금액을 줄지 말지 정한다
 4. 주의
-	= back 을 하면 money[0] -= 8 이런 식으로 해줘야 한다
+	= back 을 하면 money[i] -= 8 이런 식으로 해줘야 한다
 	= 왜? D(L + 1)로 가서 또 for 돌면서 누구한테 금액을 줄지 말지 정해야 하니까
-	= 빼앗고 money[1] += 8 이런 식으로 해줘야 한다
+**	= 빼앗고 다시 for i 돌려서 money[i+1] += 8 이런 식으로 해줘야 한다
+**	= 즉, for - (money[i] += 코인) - D(L + 1) - (money[i] -= 코인) 이런 식
 5. base case
 	= if (L == n):
+		= diff = max(money) - min(money)
+		= if (diff < result):
+			= tmp = set()
+**			= '세 사람의 총액은 서로 달라야 합니다' 문제에서 요구 -> set()해서 중복 제거 + 초기화
 
+6. 핵심
+	= if (L == n):				레벨 종착점은 모든 코인을 다 나눠줬을 때임 
+								-> 왜? 밑에 tmp = set()에 for 돌려서 .add() 박으면서 길이 다르면 다 다르게 나눠줬구나 싶은거임
+**								즉, 아~ 일단 모든 코인 다 나눠줬을 때까지로 가정하고 재귀 다해봐~~ 이따 set()으로 중복 len 체크하면 되잖어 라는 뜻!!!
+		= if (diff < result):	해당 레벨까지 간 [A][B][C] 케이스의 최대 - 최소
+			= tmp = set()		세 사람 받은 총액 다르니까 중복 제거 + 초기화
+			= for x in money: 	money [A][B][C] 한 번씩 중복인지 확인해준다
+				= tmp.add(x)	중복이 있으면 set()에 추가 안됨
+**			= if (len(tmp) == 3):	이렇게 만들어진 set()에 길이가 만약 3이 아니라면 '중복돼서 뭔가 제거 됐다'는 뜻!
+				= result = diff		아하~ 이건 중복이 없는 result구나~ 갱신!!
 
++ set()은 .add()로 추가한다 <-> 리스트는 .append()로 추가한다
 '''
 def DFS(L):
 	global result
 	if (L == n): 							# base case: 0 ~ n-1 coin 전부다 나눠줬다면 종착점
 		diff = max(money) - min(money)		# 해당 경우[A][B][C] 다 담은 최대값 - 최소값 = 차
 		if (diff < result):					# 차가 최소값보다 작다면 갱신
-			tmp = set()						# set() = 중복을 허용하지 않는다. 중복 제거
-			for x in money:
-				tmp.add(x)					# tmp에 money를 넣는다
-			if (len(tmp) == 3):				# tmp에 3개가 들어있다면, 중복이 없다면
-				result = diff				# 갱신
-	
+			tmp = set()						# set() = 세 사람 받은 총액 다르니까 중복 제거 + 초기화
+			for x in money: 				# money = [0] * n 이었는데 이는 [A][B][C]를 담은 리스트
+				tmp.add(x) 					# set()은 .add()로 추가한다 <-> 리스트는 .append()로 추가한다
+			if (len(tmp) == 3):				# 이렇게 만들어진 set()에 길이가 만약 3이 아니라면 '중복돼서 뭔가 제거 됐다'는 뜻!
+				result = diff				# 아하~ 이건 중복이 없는 result구나~ 갱신!!
 	else:
 		for i in range(3):
 			money[i] += coin[L]				# coin[L]: 해당레벨 = 8(0),9(1),11(2).. n번째 입력값
@@ -36,12 +51,13 @@ def DFS(L):
 
 n = int(input())
 coin = []
-money = [0] * n
+money = [0] * 3
 result = 2147000000							# 최소값(총액이 가장 큰 사람 - 작은 사람)을 구해야 하니까, 최대값으로 초기화
 
-for _ in range():
+for _ in range(n):
 	coin.append(int(input()))				# 왜 이렇게 할까? 엔터치면서 coin을 받기 때문
 DFS(0)
+print(result)
 
 '''
 7
