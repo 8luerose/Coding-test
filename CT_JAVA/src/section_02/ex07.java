ed592f6cde53ba5d024dbfb579237b1e3935b6eb
8luerose <8luerose@naver.com>
2024-08-29 19:13:34 +0900

    [CHORE] Add .vscode/settings.json --vscode settings for java

---
+++ CT_JAVA/src/section_02/ex07.java
@@ -0,0 +1,12 @@
+package section_02;
+import java.util.*;
+
+class ex07 {
+	public static void main(String[] args) {
+		int [] arr = {32, 55, 62, 20, 250, 370, 200, 30, 100};
+		for (int x : arr) {
+			System.out.print(x + " ");
+		}
+	}
+	
+}
