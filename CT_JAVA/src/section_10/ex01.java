795e7fbf2ab2c4ca04a646c7456771a2ae82465a
8luerose <8luerose@naver.com>
2025-03-06 12:00:00 +0900

    [FEAT] sec_10 ex01 -- DP(계단 건너기)

---
+++ CT_JAVA/src/section_10/ex01.java
@@ -0,0 +1,21 @@
+package section_10;
+import java.util.*;
+
+public class ex01 {
+	static int[] dy;
+	public static int solution(int N) {
+		dy[1] = 1;
+		dy[2] = 2;
+		for (int i = 3; i <= N; i++) {
+			dy[i] = dy[i - 2] + dy[i - 1];
+		}
+		return (dy[N]);
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		dy = new int[N + 1];
+		System.out.println(solution(N));
+		sc.close();
+	}
+}
